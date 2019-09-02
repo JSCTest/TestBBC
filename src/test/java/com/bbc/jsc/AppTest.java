a077f3b0ad48a95ea7a3fd57df622e6eb5da7fe3
Jagjeet <jagjeet_channa@hotmail.com>
2019-09-02 21:02:42 +0100

    Test method headers

--- src/test/java/com/bbc/jsc/AppTest.java
+++ src/test/java/com/bbc/jsc/AppTest.java
@@ -1,6 +1,6 @@
 package com.bbc.jsc;
 
-import org.junit.Assert;
+import org.junit.*;
 import junit.framework.TestCase;
 
 /**
@@ -8,9 +8,39 @@ import junit.framework.TestCase;
  */
 public class AppTest {
     //test input to see if separated correctly
+    @Test
+    public void testInput(){
+
+    }
+
     //test input to see if invalid input is spotted
+    @Test
+    public void testInvalidInput(){
+
+    }
+
     //test input to see if valid input is spotted
+    @Test
+    public void testValidInput(){
+
+    }
+
     //test input to see if slow/non-responsive requests are handled
+    @Test
+    public void testSlowRequest(){
+
+    }
+
     //test if properties from requests are recorded and are correct
+    @Test
+    public void testProperties(){
+
+    }
+
     //test to see if json file exists
+    @Test
+    public void testJSON(){
+
+    }
+
 }
