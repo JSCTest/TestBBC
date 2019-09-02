a33325ffdf30cd5577a8f92ff7d96e303b47ec38
Jagjeet <jagjeet_channa@hotmail.com>
2019-09-02 20:46:54 +0100

    List of Tests

--- src/test/java/com/bbc/jsc/AppTest.java
+++ src/test/java/com/bbc/jsc/AppTest.java
@@ -6,7 +6,11 @@ import junit.framework.TestCase;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-
+public class AppTest {
+    //test input to see if separated correctly
+    //test input to see if invalid input is spotted
+    //test input to see if valid input is spotted
+    //test input to see if slow/non-responsive requests are handled
+    //test if properties from requests are recorded and are correct
+    //test to see if json file exists
 }
