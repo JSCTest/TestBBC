ee92d6f30c9bf0e1bc7132a8395ba00e0bdd967d
Jagjeet <jagjeet_channa@hotmail.com>
2019-09-08 16:43:25 +0100

    Refactoring Tests and implemented additional tests

--- src/main/java/com/bbc/jsc/InputHandler.java
+++ src/main/java/com/bbc/jsc/InputHandler.java
@@ -1,5 +1,7 @@
 package com.bbc.jsc;
 
+import org.apache.commons.validator.routines.UrlValidator;
+
 import java.io.IOException;
 import java.net.*;
 import java.util.*;
@@ -25,21 +27,26 @@ public class InputHandler {
     public URLObject getProperties(String url){
         URLObject urlData=new URLObject();
         urlData.setUrl(url);
+        urlData.setValid(isValidURL(url));
         try {
             URL link = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) link.openConnection();
             connection.setRequestMethod("GET");
             connection.setConnectTimeout(10000);
             connection.connect();
-            urlData.setValid(true);
             urlData.setStatusCode(connection.getResponseCode());
             urlData.setContentLength(connection.getContentLengthLong());
             urlData.setDate(new Date(connection.getDate()).toString());
         }catch(Exception e){
-            System.err.println("The URL \""+url +"\" is invalid ");
-            urlData.setValid(false);
             urlData.setError("Invalid URL");
         }
         return urlData;
+    }
+
+    public boolean isValidURL(String url){
+        UrlValidator urlValidator = new UrlValidator();
+        if(urlValidator.isValid(url)) return true;
+        else System.err.println("The URL \""+url +"\" is invalid ");
+        return false;
     }
 }
--- src/test/java/com/bbc/jsc/AppTest.java
+++ src/test/java/com/bbc/jsc/AppTest.java
@@ -1,5 +1,6 @@
 package com.bbc.jsc;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.*;
 
 import java.io.*;
@@ -21,12 +22,52 @@ public class AppTest {
                                               "http://www.bbc.co.uk/missing/thing",
                                               "http://not.exists.bbc.co.uk/",
                                               "http://www.oracle.com/technetwork/java/javase/downloads/index.html");
+    private List<String>smallLst = Arrays.asList("http://www.bbc.co.uk/iplayer",
+                                                 "https://google.com",
+                                                 "bad://address");
+    // test getters and setters of UserInput class
+    @Test
+    public void testInputLstGetSet(){
+        UserInput userInput = new UserInput();
+        userInput.setInputLst(inArray);
+        userInput.setDocType("json");
+        Assert.assertEquals(inLst,userInput.getInputLst());
+        Assert.assertEquals("json",userInput.getDocType());
+    }
 
     // test user input to see if stored
     @Test
     public void testInputLst(){
         UserInput userInput = new UserInput();
-        userInput.setInputLst(inArray);
+        String input="http://www.bbc.co.uk/iplayer\nhttps://google.com\nbad://address\nhttp://www.bbc.co.uk/missing/thing\nhttp://not.exists.bbc.co.uk/\nhttp://www.oracle.com/technetwork/java/javase/downloads/index.html\ndone\njson";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        userInput.UserGUI();
+        System.setIn(System.in);
+        Assert.assertEquals(inLst,userInput.getInputLst());
+        Assert.assertEquals("json",userInput.getDocType());
+    }
+
+    // test empty input for URL
+    @Test
+    public void testEmptyURL(){
+        UserInput userInput = new UserInput();
+        String input="http://www.bbc.co.uk/iplayer\n\nhttps://google.com\nbad://address\n\n\nhttp://www.bbc.co.uk/missing/thing\nhttp://not.exists.bbc.co.uk/\nhttp://www.oracle.com/technetwork/java/javase/downloads/index.html\n\ndone\njson";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        userInput.UserGUI();
+        System.setIn(System.in);
+        Assert.assertEquals(inLst,userInput.getInputLst());
+    }
+
+    // test duplicate input for URL
+    @Test
+    public void testDuplicateURL(){
+        UserInput userInput = new UserInput();
+        String input="http://www.bbc.co.uk/iplayer\nhttps://google.com\nbad://address\nhttps://google.com\nhttp://www.bbc.co.uk/missing/thing\nhttp://not.exists.bbc.co.uk/\nhttp://www.oracle.com/technetwork/java/javase/downloads/index.html\n" +
+                "http://www.bbc.co.uk/missing/thing\n" +
+                "http://www.bbc.co.uk/missing/thing\nbad://address\ndone\njson";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        userInput.UserGUI();
+        System.setIn(System.in);
         Assert.assertEquals(inLst,userInput.getInputLst());
     }
 
@@ -34,23 +75,35 @@ public class AppTest {
     @Test
     public void testInvalidInput(){
         InputHandler handler =new InputHandler(inLst);
-        Assert.assertEquals(false,handler.getProperties("bad://address").getValid());
-        Assert.assertEquals(false,handler.getProperties("https:// www.google.com").getValid());
+        Assert.assertEquals(false,handler.isValidURL("bad://address"));
+        Assert.assertEquals(false,handler.isValidURL("https:// www.google.com"));
     }
 
     //test input to see if valid input is spotted
     @Test
     public void testValidInput(){
         InputHandler handler =new InputHandler(inLst);
-        Assert.assertEquals(true,handler.getProperties("https://google.com").getValid());
-        Assert.assertEquals(true,handler.getProperties("http://www.bbc.co.uk/iplayer").getValid());
-        Assert.assertEquals(true,handler.getProperties("http://www.oracle.com/technetwork/java/javase/downloads/index.html").getValid());
+        Assert.assertEquals(true,handler.isValidURL("https://google.com"));
+        Assert.assertEquals(true,handler.isValidURL("http://www.bbc.co.uk/iplayer"));
+        Assert.assertEquals(true,handler.isValidURL("http://www.oracle.com/technetwork/java/javase/downloads/index.html"));
     }
 
-    //test input to see if slow/non-responsive requests are handled
+    //test URLObject class
     @Test
-    public void testSlowRequest(){
+    public void testURLObject(){
+        String date=new Date().toString();
+        URLObject validUrlObject = new URLObject("https://google.com",200,1234,date,true);
+        URLObject invalidUrlObject = new URLObject("bad://address",false,"Invalid URL");
 
+        Assert.assertEquals("https://google.com",validUrlObject.getUrl());
+        Assert.assertEquals(200,validUrlObject.getStatusCode());
+        Assert.assertEquals(1234,validUrlObject.getContentLength());
+        Assert.assertEquals(date,validUrlObject.getDate());
+        Assert.assertEquals(true,validUrlObject.getValid());
+
+        Assert.assertEquals("bad://address",invalidUrlObject.getUrl());
+        Assert.assertEquals(false,invalidUrlObject.getValid());
+        Assert.assertEquals("Invalid URL",invalidUrlObject.getError());
     }
 
     //test if properties from requests are recorded and are correct
@@ -66,7 +119,6 @@ public class AppTest {
 
             //Get Properties
             int code=connection.getResponseCode();
-            String date = new Date(connection.getDate()+1).toString();
 
             //Get Property value from method
             InputHandler handler =new InputHandler(inLst);
@@ -76,7 +128,7 @@ public class AppTest {
             Assert.assertEquals(url,properties.getUrl());
             Assert.assertEquals(true,properties.getValid());
             Assert.assertEquals(code,properties.getStatusCode());
-            Assert.assertEquals(date,properties.getDate());
+
 
 
             //---------------- Connect to 2nd URL ----------------
@@ -88,7 +140,6 @@ public class AppTest {
 
             //Get Properties
             code=connection.getResponseCode();
-            date = new Date(connection.getDate()).toString();
 
             //Get Property value from method
             properties=handler.getProperties(url);
@@ -97,7 +148,6 @@ public class AppTest {
             Assert.assertEquals(url,properties.getUrl());
             Assert.assertEquals(true,properties.getValid());
             Assert.assertEquals(code,properties.getStatusCode());
-            Assert.assertEquals(date,properties.getDate());
 
 
             //---------------- Connect to 3rd URL ----------------
@@ -115,42 +165,74 @@ public class AppTest {
 
     }
 
-    //test to see if json file exists
+    //test DocFactory class to see if correct object returned
     @Test
-    public void testJSON(){
-        //Get the data JSON file should contain
-        List<String>links = Arrays.asList("https://www.bbc.co.uk/iplayer","https://google.com","bad://address");
-        InputHandler handler =new InputHandler(links);
-        List<URLObject>lstOfProperties=new LinkedList<>();
-        for(String url:links) {
-            URLObject urlObject = handler.getProperties(url);
-            lstOfProperties.add(urlObject);
-        }
-
-        //test to make sure file doesn't exist yet
-        File file = new File("Response_Count.json");
-        file.delete();
-        Assert.assertEquals(false,file.exists());
-
-        //Create the JSON Document
-        JSONDocument jsonDocument=new JSONDocument();
-        jsonDocument.convert(lstOfProperties);
-
-        //test to see if file has been created
-        Assert.assertEquals(true,file.exists());
+    public void testDocFactory(){
+        Document document=new DocumentFactory().getDocument("json");
+        boolean isCorrect=false;
+        if(document instanceof JSONDocument) isCorrect=true;
+        Assert.assertEquals(true,isCorrect);
     }
 
     //test to see if json file exists
     @Test
-    public void testJSONRead(){
+    public void testJSONExists(){
+
         //Get the data JSON file should contain
-        List<String>links = Arrays.asList("https://www.bbc.co.uk/iplayer","https://google.com","bad://address");
-        InputHandler handler =new InputHandler(links);
+        String date=new Date().toString();
         List<URLObject>lstOfProperties=new LinkedList<>();
-        for(String url:links) {
-            URLObject urlObject = handler.getProperties(url);
-            lstOfProperties.add(urlObject);
+        URLObject validUrlObject = new URLObject("https://google.com",200,1234,date,true);
+        URLObject validUrlObject2 = new URLObject("https://www.bbc.co.uk/iplayer",300,134,date,true);
+        URLObject invalidUrlObject = new URLObject("bad://address",false,"Invalid URL");
+        lstOfProperties.add(validUrlObject);
+        lstOfProperties.add(validUrlObject2);
+        lstOfProperties.add(invalidUrlObject);
+
+        //test to make sure response file doesn't exist yet
+        File file = new File("Response_Count.json");
+        file.delete();
+        Assert.assertEquals(false,file.exists());
+
+        //test to make sure LinkData directory is empty
+        File fileURLs = new File("LinkData");
+        try {
+            FileUtils.cleanDirectory(fileURLs);
+        } catch(IllegalArgumentException e){
+            System.err.println("Directory doesn't exist\nCreating directory");
+            fileURLs.mkdir();
         }
+        catch (IOException e) {
+            System.err.println("Unsuccessful cleaning of directory");
+        }
+        Assert.assertEquals(0,fileURLs.listFiles().length);
+
+        //Create the JSON Documents
+        JSONDocument jsonDocument=new JSONDocument();
+        jsonDocument.convert(lstOfProperties);
+
+        //test to see if files have been created
+        Assert.assertEquals(true,file.exists());
+        Assert.assertEquals(3,fileURLs.listFiles().length);
+    }
+
+
+
+
+
+
+    //test to see if json file contents
+    @Test
+    public void testJSONRead(){
+
+        //Get the data JSON file should contain
+        String date=new Date().toString();
+        List<URLObject>lstOfProperties=new LinkedList<>();
+        URLObject validUrlObject = new URLObject("https://google.com",200,1234,date,true);
+        URLObject validUrlObject2 = new URLObject("https://www.bbc.co.uk/iplayer",300,134,date,true);
+        URLObject invalidUrlObject = new URLObject("bad://address",false,"Invalid URL");
+        lstOfProperties.add(validUrlObject);
+        lstOfProperties.add(validUrlObject2);
+        lstOfProperties.add(invalidUrlObject);
 
         //Create the JSON Document
         Document jsonDocument=new JSONDocument();
@@ -170,6 +252,37 @@ public class AppTest {
                 Assert.assertEquals(lstOfProperties.get(i).getUrl(),lstOfJSONLinks.get(i).getUrl());
                 Assert.assertEquals(lstOfProperties.get(i).getError(),lstOfJSONLinks.get(i).getError());
             }
+        }
+    }
+
+    //test to see if json file contents
+    @Test
+    public void testJSONResponseRead(){
+
+        //Creating data for JSON files
+        String date=new Date().toString();
+        List<URLObject>lstOfProperties=new LinkedList<>();
+        URLObject validUrlObject = new URLObject("https://google.com",200,1234,date,true);
+        URLObject validUrlObject2 = new URLObject("https://www.bbc.co.uk/iplayer",300,134,date,true);
+        URLObject validUrlObject3 = new URLObject("http://www.oracle.com/technetwork/java/javase/downloads/index.html",300,1344,date,true);
+        URLObject invalidUrlObject = new URLObject("bad://address",false,"Invalid URL");
+        lstOfProperties.add(validUrlObject);
+        lstOfProperties.add(validUrlObject2);
+        lstOfProperties.add(invalidUrlObject);
+
+        //Creating map to contain data response doc should contain
+        Map<Integer,Integer>expResponse=new LinkedHashMap<>();
+        expResponse.put(200,1);
+        expResponse.put(300,1);
+
+        //Create the JSON Document
+        JSONDocument jsonDocument=new JSONDocument();
+        jsonDocument.convert(lstOfProperties);
+        Map<Integer,Integer>lstOfResponse= jsonDocument.readResponse();
+
+        //Check if data from JSON file matches the actual data
+        for (int code:expResponse.keySet()){
+            Assert.assertEquals(expResponse.get(code),lstOfResponse.get(code));
         }
     }
 
